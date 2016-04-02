8a94d59f6fd8fae1a38ae9ab5492e371f0f9ec2a
Srini <srinigopishetty@gmail.com>
2016-04-02 11:22:45 -0400

    Initialize repo

---
+++ server.js
@@ -0,0 +1,31 @@
+var PORT = 9001;
+var express = require('express');
+var app = express();
+
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('Private Route!');
+		next();
+	},
+	logger: function (req, res, next){
+		console.log('Request: ' + new Date().toString()  + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+}
+
+app.use(middleware.logger);
+
+app.get('/about', function(req, res) {
+res.send('Hello About what!	');
+});
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(PORT, function (){
+	console.log('Express Server Started on port ' + PORT);
+});
+
